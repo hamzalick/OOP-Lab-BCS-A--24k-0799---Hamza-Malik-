43f9e03e67b24a8b4ba77d2de5c3b2db14d7b314
hamzalick <k240799@nu.edu.pk>
2025-09-17 09:34:25 +0500

    Create Q3.cpp

---
+++ lab2/Q3.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int sumArray(int *arr, int size) {
+    int sum = 0;
+    for (int i = 0; i < size; i++) {
+        sum += *(arr + i);
+    }
+    return sum;
+}
+
+int main() {
+    int size;
+    cout << "Enter size of array: ";
+    cin >> size;
+
+    int arr[size];
+    cout << "Enter " << size << " elements: ";
+    for (int i = 0; i < size; i++) {
+        cin >> arr[i];
+    }
+
+    cout << "Sum of array elements: " << sumArray(arr, size) << endl;
+    return 0;
+}
