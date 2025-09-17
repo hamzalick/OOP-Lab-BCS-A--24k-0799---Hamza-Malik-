a0b8427906e00f60502d007376cea8ddc2a92908
hamzalick <k240799@nu.edu.pk>
2025-09-17 21:45:16 +0500

    Update Q4.cpp

--- lab2/Q4.cpp
+++ lab2/Q4.cpp
@@ -1,4 +1,44 @@
 #include <iostream>
 using namespace std;
 
+int main() {
+    int N;
+    cout << "Enter size of square matrix: ";
+    cin >> N;
 
+    int **matrix = new int*[N];
+    for (int i = 0; i < N; i++) {
+        matrix[i] = new int[N];
+    }
+
+    cout << "Enter elements of the matrix:" << endl;
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            cin >> matrix[i][j];
+        }
+    }
+
+    int mainDiagonalSum = 0, secondaryDiagonalSum = 0;
+    for (int i = 0; i < N; i++) {
+        mainDiagonalSum += matrix[i][i];
+        secondaryDiagonalSum += matrix[i][N - i - 1];
+    }
+
+    cout << "Matrix:" << endl;
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            cout << matrix[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    cout << "Sum of main diagonal: " << mainDiagonalSum << endl;
+    cout << "Sum of secondary diagonal: " << secondaryDiagonalSum << endl;
+
+    for (int i = 0; i < N; i++) {
+        delete[] matrix[i];
+    }
+    delete[] matrix;
+
+    return 0;
+}
