00f62b153491928135b19641dcbd2d4e45278b53
hamzalick <k240799@nu.edu.pk>
2025-09-17 21:36:15 +0500

    Create Q4.cpp

---
+++ lab3/Q4.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+class Calculator {
+private:
+    int num1, num2;
+public:
+    void setNumbers(int a, int b);
+    int add();
+};
+
+void Calculator::setNumbers(int a, int b) {
+    num1 = a;
+    num2 = b;
+}
+
+int Calculator::add() {
+    return num1 + num2;
+}
+
+int main() {
+    Calculator c;
+    c.setNumbers(15, 25);
+    cout << "Sum: " << c.add() << endl;
+    return 0;
+}
