c82b1e6688c78174b0db7492c34217dc0890db3c
hamzalick <k240799@nu.edu.pk>
2025-09-17 21:30:27 +0500

    Update Q1.cpp

--- lab3/Q1.cpp
+++ lab3/Q1.cpp
@@ -1,1 +1,18 @@
+#include <iostream>
+using namespace std;
 
+class Student {
+public:
+    string name;
+    int rollNumber;
+};
+
+int main() {
+    Student s;
+    s.name = "Ali";
+    s.rollNumber = 101;
+
+    cout << "Name: " << s.name << endl;
+    cout << "Roll Number: " << s.rollNumber << endl;
+    return 0;
+}
